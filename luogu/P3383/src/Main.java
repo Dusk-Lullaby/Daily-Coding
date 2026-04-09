c9dbf0c216e12759907687c5a69b1aa915b71715
Sonnet <Sonnet@Sonnet14.cn>
2026-04-09 15:18:35 +0800

    first commit

---
+++ luogu/P3383/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 100;
+        boolean[] isPrime = new boolean[n + 1];
+        int count = 0;
+        int[] prime = new int[n + 1];
+
+
+    }
+}
