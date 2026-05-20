159e0011f1f3b4a02a5b9ab8871ceaca34388a1b
Sonnet <2278521104@qq.com>
2026-05-20 12:38:47 +0800

    2026/5/20刷题

---
+++ study/src/com/lullaby/dp/linear_dp/luogu_b3637/Main.java
@@ -0,0 +1,31 @@
+package com.lullaby.dp.linear_dp.luogu_b3637;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] number = new int[n];
+        for (int i = 0; i < n; i++) {
+            number[i] = scanner.nextInt();
+        }
+        int[] DP = new int[n];
+        Arrays.fill(DP, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (number[i] > number[j]) {
+                    DP[i] = Math.max(DP[j] + 1, DP[i]);
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, DP[i]);
+        }
+
+        System.out.println(ans);
+    }
+}
