0dcf6dd514b0f8ad489dba1399101040a1281d55
Sonnet <2278521104@qq.com>
2026-05-23 16:00:00 +0800

    替换

--- study/src/com/lullaby/dp/region_dp/lgp1470/Main.java
+++ study/src/com/lullaby/dp/region_dp/lgp1470/Main.java
@@ -1,4 +1,32 @@
 package com.lullaby.dp.region_dp.lgp1470;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.next();
+        char[] chars = string.toCharArray();
+        int[][] dp = new int[chars.length][chars.length];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, 0x3f3f3f3f);
+        }
+        for (int i = 0; i < chars.length; i++) {
+            dp[i][i] = 1;
+        }
+        for (int len = 2; len <= chars.length; len++) {
+            for (int i = 0; i <= chars.length - len; i++) {
+                int j = i + len - 1;
+                if (chars[i] == chars[j]) {
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i + 1][j]);
+                } else {
+                    for (int k = i; k < j; k++) {
+                        dp[i][j] = Math.min(dp[i][k] + dp[k + 1][j], dp[i][j]);
+                    }
+                }
+            }
+        }
+        System.out.println(dp[0][chars.length - 1]);
+    }
 }
