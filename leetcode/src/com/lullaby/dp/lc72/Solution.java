f15a27d6e5a8efc84921c2b32935cf54e39ce7dd
Sonnet <2278521104@qq.com>
2026-06-02 20:41:18 +0800

    2026/6/2刷题

---
+++ leetcode/src/com/lullaby/dp/lc72/Solution.java
@@ -0,0 +1,25 @@
+package com.lullaby.dp.lc72;
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        word1 = " " + word1;
+        word2 = " " + word2;
+        int[][] dp = new int[word1.length()][word2.length()];
+        for (int i = 0; i < word1.length(); i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i < word2.length(); i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i< word1.length(); i++) {
+            for (int j = 1; j < word2.length() ;j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+                }
+            }
+        }
+        return dp[word1.length() - 1][word2.length() - 1];
+    }
+}
---
+++ leetcode/src/com/lullaby/dp/lc72/Test.java
@@ -0,0 +1,13 @@
+package com.lullaby.dp.lc72;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        String word1 = "horse";
+//        String word2 = "ros";
+        String word1 = "a";
+        String word2 = "ab";
+        System.out.println(solution.minDistance(word1, word2));
+    }
+}
