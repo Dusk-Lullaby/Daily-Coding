5fdf435746b962893d9c81f8010a1827fa8861cc
Sonnet <2278521104@qq.com>
2026-06-03 20:52:24 +0800

    2026/6/3刷题

---
+++ leetcode/src/com/lullaby/dp/lc95/Solution.java
@@ -0,0 +1,38 @@
+package com.lullaby.dp.lc95;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    class Solution {
+        public List<TreeNode> generateTrees(int n) {
+            List<List<TreeNode>> allTree = new ArrayList<>();
+            for (int i = 0; i <= n; i++) {
+                allTree.add(new ArrayList<>());
+            }
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    for (TreeNode left : allTree.get(j - 1)) {
+                        for (TreeNode right : allTree.get(i - j)) {
+                            allTree.get(i).add(new TreeNode(i, left, right));
+                        }
+                    }
+                }
+            }
+
+            return allTree.get(n);
+        }
+    }
+}
+
---
+++ leetcode/src/com/lullaby/dp/lc96/Test.java
@@ -0,0 +1,9 @@
+package com.lullaby.dp.lc96;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.numTrees(3));
+    }
+}
