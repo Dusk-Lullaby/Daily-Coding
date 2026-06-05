343573b605d2cee48f59f7b42da2187a6768b232
Sonnet <2278521104@qq.com>
2026-06-05 20:58:59 +0800

    修改错误

--- leetcode/src/com/lullaby/dp/lc95/Solution.java
+++ leetcode/src/com/lullaby/dp/lc95/Solution.java
@@ -3,36 +3,43 @@ package com.lullaby.dp.lc95;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TreeNode {
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
     }
+}
 
-    class Solution {
-        public List<TreeNode> generateTrees(int n) {
-            List<List<TreeNode>> allTree = new ArrayList<>();
-            for (int i = 0; i <= n; i++) {
-                allTree.add(new ArrayList<>());
-            }
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= i; j++) {
-                    for (TreeNode left : allTree.get(j - 1)) {
-                        for (TreeNode right : allTree.get(i - j)) {
-                            allTree.get(i).add(new TreeNode(i, left, right));
-                        }
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        List<List<TreeNode>> allTree = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            allTree.add(new ArrayList<>());
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                for (TreeNode left : allTree.get(j - 1)) {
+                    for (TreeNode right : allTree.get(i - j)) {
+                        allTree.get(i).add(new TreeNode(i, left, right));
                     }
                 }
             }
-
-            return allTree.get(n);
         }
+
+        return allTree.get(n);
     }
 }
+
 
